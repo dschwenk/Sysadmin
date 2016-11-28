489a605bc024b0be87bc1211762656e2bfdc50ce
Daniel Schwenk <mail@danielschwenk.de>
2016-11-28 22:13:44 +0100

    updated doku - ip address analysis

--- Dokumentation/Dokumentation/listings/do_all.sh
+++ Dokumentation/Dokumentation/listings/do_all.sh
@@ -2,14 +2,21 @@
 #
 # set variables
 DATE=`date +%Y%m%d_%H-%M`
-BACUP_FILENAME="/home/dschwenk/backup/backup.tar.gz"
-PIPAL_OUTPUT="/home/dschwenk/pipal/pipal-stats.txt"
+BACKUP_FILENAME="/home/dschwenk/backup/"$DATE"_backup.tar.gz"
+
 BACKUP_KIPPO_LOG="/home/kippo/kippo/log/kippo.log"
+BACKUP_KIPPO_CONF="/home/kippo/kippo/kippo.cfg"
+BACKUP_KIPPO_DATA="/home/dschwenk/kippo-data"
+
+PIPAL_OUTPUT="/home/dschwenk/pipal/pipal-stats.txt"
+
 BACKUP_FIREWALL="/home/dschwenk/firewall/iptables-backup.txt"
-#BACUP_FILENAME="/home/dschwenk/backup/$DATE_backup.tar.gz"
 
 # start Kippo logfile data extract
 /home/dschwenk/kippo-data/extract-logifile-data.sh
+
+# do reverse-DNS lookup
+/home/dschwenk/kippo-data/do_reverse_dns_lookup.sh
 
 # do Pipal password analysis
 /home/dschwenk/pipal/pipal.rb --output $PIPAL_OUTPUT /home/dschwenk/kippo-data/pw.txt
@@ -19,10 +26,7 @@ BACKUP_FIREWALL="/home/dschwenk/firewall/iptables-backup.txt"
 
 # do backup stuff
 # create zip
-tar -czvf $BACUP_FILENAME $BACKUP_KIPPO_LOG $PIPAL_OUTPUT $BACKUP_FIREWALL
+tar czPf $BACKUP_FILENAME $BACKUP_KIPPO_LOG $BACKUP_KIPPO_CONF $BACKUP_KIPPO_DATA $PIPAL_OUTPUT $BACKUP_FIREWALL
 
 # upload backup
-gdrive upload $BACUP_FILENAME
-
-# cleanup
-
+gdrive upload $BACKUP_FILENAME
