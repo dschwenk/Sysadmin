c5fd874fbf80e40189aa8b9dc0b9ecf2ea2a2ed7
FurchtBar283 <ms-131871@hs-weingarten.de>
2016-12-15 20:35:48 +0100

    Added much more stuff, added backup for snare-logfiles to the do_all script

--- Dokumentation/Dokumentation/listings/do_all.sh
+++ Dokumentation/Dokumentation/listings/do_all.sh
@@ -7,8 +7,11 @@ BACKUP_FILENAME="/home/dschwenk/backup/"$DATE"_backup.tar.gz"
 BACKUP_KIPPO_LOG="/home/kippo/kippo/log/kippo.log"
 BACKUP_KIPPO_CONF="/home/kippo/kippo/kippo.cfg"
 BACKUP_KIPPO_DATA="/home/dschwenk/kippo-data"
+BACKUP_SNARE_LOG="/home/mstroh/snare/snare.log"
 
 PIPAL_OUTPUT="/home/dschwenk/pipal/pipal-stats.txt"
+BACKUP_SNARE_POST="/home/mstroh/logs/snare-post.txt"
+BACKUP_SNARE_REQUEST_PATHS="/home/mstroh/logs/snare-request-paths.txt"
 
 BACKUP_FIREWALL="/home/dschwenk/firewall/iptables-backup.txt"
 
@@ -24,9 +27,12 @@ BACKUP_FIREWALL="/home/dschwenk/firewall/iptables-backup.txt"
 # do firewall stuff
 /home/dschwenk/firewall/create-iptable-rules.sh
 
+# extract SNARE logfile data
+/home/mstroh/extract_snare_logfile_entries.sh
+
 # do backup stuff
 # create zip
-tar czPf $BACKUP_FILENAME $BACKUP_KIPPO_LOG $BACKUP_KIPPO_CONF $BACKUP_KIPPO_DATA $PIPAL_OUTPUT $BACKUP_FIREWALL
+tar czPf $BACKUP_FILENAME $BACKUP_KIPPO_LOG $BACKUP_KIPPO_CONF $BACKUP_KIPPO_DATA $PIPAL_OUTPUT $BACKUP_FIREWALL $BACKUP_SNARE_LOG $BACKUP_SNARE_POST $BACKUP_SNARE_REQUEST_PATHS
 
 # upload backup
 gdrive upload $BACKUP_FILENAME
